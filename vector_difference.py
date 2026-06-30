7855df0560c00f98cba1a9342e519207bb802065
JamieMacManaway <jamiemacmanaway@gmail.com>
2026-06-30 10:38:29 +0100

    added .gitignore for data folder and added difference.py

--- vector_difference.py
+++ vector_difference.py
@@ -1,5 +1,13 @@
 import geopandas as gpd
 
+# define a function that takes two shapefiles and returns the area where they don't overlap
+def vector_difference(input_shape, overlay_shape, projection):
+    input_layer = gpd.read_file(input_shape).to_crs(epsg=projection)
+    overlay_layer = gpd.read_file(overlay_shape).to_crs(epsg=projection)
+
+    difference_layer = input_layer.overlay(overlay_layer, how='difference')
+    return difference_layer
+
 scotland = gpd.read_file('Project/Data/vectors/Scotland/scotland.shp')
 lochs = gpd.read_file('Project/Data/vectors/Scotland/scotlochs.shp') 
 
