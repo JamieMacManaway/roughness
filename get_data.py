e582718075e6aa7325c80149f4f5da2d737b6934
JamieMacManaway <jamiemacmanaway@gmail.com>
2026-06-30 12:02:06 +0100

    tidying up

--- get_data.py
+++ get_data.py
@@ -4,19 +4,23 @@ from pathlib import Path
 import geopandas as gpd
 import tarfile
 
-# specify a folder in which to save ArcticDEM tiles
-folder = Path('Project/Data/dems/Greenland')
-folder.mkdir(parents=True, exist_ok=True)
+# read in the greenland boundary, ice sheet and ArcticDEM mosaic index shapefiles
+boundary = gpd.read_file('data/vectors/greenland/GRL_adm0.shp').to_crs(epsg=3413)
+ice_sheet = gpd.read_file('data/vectors/greenland/GRE_IceSheet_IMBIE2_v1.shp').to_crs(epsg=3413)
+index = gpd.read_file('data/vectors/greenland/ArcticDEM_Mosaic_Index_v4_1_2m.shp').to_crs(epsg=3413)
 
-# read in the exposed land and ArcticDEM mosaic index shapefiles
-land = gpd.read_file('Project/Data/vectors/Greenland/exposed_land.shp').to_crs(epsg=3413)
-index = gpd.read_file('Project/Data/vectors/Greenland/ArcticDEM_Mosaic_Index_v4_1_2m.shp').to_crs(epsg=3413)
+# find area of Greenland surrounding the ice sheet
+periphery = boundary.overlay(ice_sheet, how='difference')
 
-# extract tiles from the index which correspond to exposed land
+# locate tiles from the index which correspond to the periphery
 gland_index = index[index.intersects(land.union_all())]
 
 # extract corresponding urls for tiles of interest 
 tiles = list(gland_index['fileurl'])
+
+# specify a folder in which to save ArcticDEM tiles
+folder = Path('data/rasters/greenland/dems')
+folder.mkdir(parents=True, exist_ok=True)
 
 # function which downloads the tiles of interest into the previously specified folder, only if they don't already exist
 def get_data(tiles):
@@ -54,6 +58,7 @@ def check_tar_integrity(tar_path):
 
 # Iterate through all .tar files in the directory and check their integrity
 # If any files corrupted, run the script again to ensure completeness of dataset
+# TODO automate the process so that if any files are corrupted, the get_data function runs again until the dataset is complete
 for tar_file in folder.rglob('*'):
     if tar_file.suffix in [".tar", ".gz"]:  # Check for .tar or .gz files
         print(f"Checking {tar_file.name}...")
