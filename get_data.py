1b31c676b5091ad2ff02d29971527bb33d5769c1
JamieMacManaway <jamiemacmanaway@gmail.com>
2026-07-01 10:40:03 +0100

    removed sandbox scripts in favour of branching. Made a branch!

--- get_data.py
+++ get_data.py
@@ -3,6 +3,7 @@ import subprocess
 from pathlib import Path
 import geopandas as gpd
 import tarfile
+from osgeo import gdal
 
 # read in the greenland boundary, ice sheet and ArcticDEM mosaic index shapefiles
 boundary = gpd.read_file('data/vectors/greenland/GRL_adm0.shp').to_crs(epsg=3413)
@@ -35,7 +36,10 @@ def get_data(tiles):
         subprocess.run(cmd, check=True)
 
 # run the function and download the tiles of interest
-get_data(tiles)
+# get_data(tiles) #commented out for testing
+
+short = tiles[:3]
+get_data(short)
 
 # function to check that downloaded files are complete and uncorrupted
 # If any files corrupted, runs the download function again to ensure completeness of dataset
@@ -60,3 +64,44 @@ for tar_file in tar_folder.rglob('*'):
     if tar_file.suffix in [".tar", ".gz"]:  # Check for .tar or .gz files
         print(f"Checking {tar_file.name}...")
         check_tar_integrity(tar_file)
+
+# create a new folder into which the dems will be unpacked
+unpacked_folder = Path('data/rasters/greenland/unpacked')
+unpacked_folder.mkdir(parents=True, exist_ok=True)
+
+# create a new folder into which the dems will be deflated (necessary for whitebox operations)
+dem_folder = Path('data/rasters/greenland/dems')
+dem_folder.mkdir(parents=True, exist_ok=True)
+
+# iterate through the tar files and unpack the dems into the previously specified folder
+for tar_file in tar_folder.iterdir():
+    with tarfile.open(tar_file, 'r') as tar:
+        members = tar.getmembers()
+        for member in members:
+            if 'dem.tif' in member.name:
+                print(f'unpacking {member.name}')
+                dem = tar.extract(member, path=unpacked_folder)
+                deflater = gdal.Open(dem)
+                options = ['COMPRESS=DEFLATE']
+                gdal.Translate(dem_folder / member.name, deflater, creationOptions=options)
+                print(f'{member.name} unpacked successfully')
+
+    #     tif_files = list(Path(input_dir).rglob("*dem.tif"))
+    # for resolution in resolutions:
+    #     for tif_file in tif_files:
+    #         if f'_{resolution}_' in tif_file.name:
+    #             resolution_dir = destination_folder / resolution
+    #             resolution_dir.mkdir(parents=True, exist_ok=True)  # Ensure folder exists
+    #             target_path = resolution_dir / tif_file.name
+                
+    #             # Open the input file
+    #             ds = gdal.Open(tif_file)
+                
+    #             # Define the output options, including deflation
+    #             options = ['COMPRESS=DEFLATE']
+                
+    #             # Create the output file with the same data but with deflation
+    #             gdal.Translate(target_path, ds, creationOptions=options)
+
+    #             #tif_file.rename(target_path)
+    #             print(f"Moved {tif_file} to {target_path}")
