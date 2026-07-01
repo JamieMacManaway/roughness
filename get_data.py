e79eed1eef8465bb68530d1e43816e04af048bb0
JamieMacManaway <jamiemacmanaway@gmail.com>
2026-07-01 13:58:34 +0100

    added print statements to let the user know what is going on whilst the code runs.

--- get_data.py
+++ get_data.py
@@ -7,6 +7,7 @@ from osgeo import gdal
 import shutil
 
 # read in the greenland boundary, ice sheet and ArcticDEM mosaic index shapefiles
+print('Reading in data and identifying area of interest...')
 boundary = gpd.read_file('data/vectors/greenland/GRL_adm0.shp').to_crs(epsg=3413)
 ice_sheet = gpd.read_file('data/vectors/greenland/GRE_IceSheet_IMBIE2_v1.shp').to_crs(epsg=3413)
 index = gpd.read_file('data/vectors/greenland/ArcticDEM_Mosaic_Index_v4_1_2m.shp').to_crs(epsg=3413)
@@ -19,6 +20,7 @@ gland_index = index[index.intersects(periphery.union_all())]
 
 # extract corresponding urls for tiles of interest 
 tiles = list(gland_index['fileurl'])
+print('Found the relevant tiles. Downloading now...')
 
 # specify a folder in which to save downloaded ArcticDEM tiles
 tar_folder = Path('data/rasters/greenland/tars')
@@ -61,10 +63,12 @@ def check_tar_integrity(tar_path):
         get_data(tiles)
 
 # Iterate through all .tar files in the directory and check their integrity
+print('Checking downloaded tar files...')
 for tar_file in tar_folder.rglob('*'):
     if tar_file.suffix in [".tar", ".gz"]:  # Check for .tar or .gz files
         print(f"Checking {tar_file.name}...")
         check_tar_integrity(tar_file)
+print('All tars downloaded successfully. Preparing to unpack dems...')
 
 # create a new folder into which the dems will be unpacked
 unpacked_folder = Path('data/rasters/greenland/unpacked')
@@ -92,3 +96,4 @@ for tar_file in tar_folder.iterdir():
                     print(f'{member.name} unpacked successfully')
 
 shutil.rmtree(unpacked_folder)
+print('Greenland data ready for further analysis.')
