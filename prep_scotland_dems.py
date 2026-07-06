35046eb8844b2e420493511ae7d1e3fdbb5cdc80
JamieMacManaway <jamiemacmanaway@gmail.com>
2026-07-06 16:12:24 +0100

    finished scottish data prep script

---
+++ prep_scotland_dems.py
@@ -0,0 +1,42 @@
+# import required libraries
+from osgeo import gdal
+from pathlib import Path
+import shutil
+
+# locate directory containing Scottish dataset
+asc_folder = Path('data/rasters/scotland/ascs')
+
+# create directories for pre-processing
+warp_folder = Path('data/rasters/scotland/warped')
+warp_folder.mkdir(parents=True, exist_ok=True)
+
+dem_folder = Path('data/rasters/scotland/dems')
+dem_folder.mkdir(parents=True, exist_ok=True)
+
+# reproject data to epsg 277700
+for asc in asc_folder.rglob('*.asc'):
+    if (warp_folder / asc.name).exists():
+        print(f'{asc.name} already processed. Skipping...')
+        continue
+    else:
+        print(f'Reprojecting {asc.name}...')
+        gdal.Warp(str(warp_folder / asc.name), asc, dstSRS='EPSG:27700')
+        print(f'{asc.name} reprojected successfully.')
+
+# convert ascs to tifs
+for file in warp_folder.rglob('*.asc'):
+    if Path(dem_folder / file.stem).with_suffix('.tif').is_file():
+        print(f'{file.name} already processed. Skipping...')
+        continue
+    else:
+        print(f'Converting {file.name}...')
+        gdal.Translate((dem_folder / file.stem).with_suffix('.tif'), file, format='GTiff')
+        print(f'{file.name} converted successfully.')
+
+print('')
+print('Tidying up...')
+print('')
+
+# remove extraneous directory
+shutil.rmtree(warp_folder)
+print('Scotland data ready for further analysis.')
