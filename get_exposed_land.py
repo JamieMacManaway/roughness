cddccd52cffedd4a15ec80ff721e49887be70271
JamieMacManaway <jamiemacmanaway@gmail.com>
2026-07-13 16:55:59 +0100

    renamed some files and added script for processing climate data

--- get_exposed_land.py
+++ get_exposed_land.py
@@ -3,18 +3,22 @@ import geopandas as gpd
 import pandas as pd
 
 # read in datasets
+# scotland
 scotland = gpd.read_file('data/vectors/scotland/scotland.shp').to_crs(epsg=27700).dissolve()
 lochs = gpd.read_file('data/vectors/scotland/GLAKES_eu.shp').to_crs(epsg=27700)
 
+# greenland
 greenland = gpd.read_file('data/vectors/greenland/GRL_adm0.shp').to_crs(epsg=3413)
 glaciers = gpd.read_file('data/vectors/greenland/RGI2000-v7.0-G-05_greenland_periphery.shp').to_crs(epsg=3413)
 ice_sheet = gpd.read_file('data/vectors/greenland/GRE_IceSheet_IMBIE2_v1.shp').to_crs(epsg=3413)
-# merge the two relevant GLAKES datasets as they're read in
+# merge the two GLAKES datasets as they're read in
 glakes = pd.concat([gpd.read_file('data/vectors/greenland/GLAKES_na1.shp').to_crs(epsg=3413), gpd.read_file('data/vectors/greenland/GLAKES_na2.shp').to_crs(epsg=3413)])
 hlakes = gpd.read_file('data/vectors/greenland/HydroLAKES_polys_v10.shp').to_crs(epsg=3413)
 
 # remove areas covered by lochs in Scotland
 lochless = scotland.overlay(lochs, how='difference')
+
+# save scotland dataset to file
 lochless.to_file('data/vectors/scotland/lochfree.shp')
 
 # merge the hlakes dataset above 76 degrees latitude with the glakes dataset below
@@ -25,4 +29,5 @@ greenland_without_ice_sheet = greenland.overlay(ice_sheet, how='difference')
 greenland_without_ice = greenland_without_ice_sheet.overlay(glaciers, how='difference')
 greenland_wihout_ice_or_water = greenland_without_ice.overlay(lakes, how='difference')
 
+# save greenland dataset to file
 greenland_without_ice_or_water.to_file('data/vectors/greenland/exposed_land.shp')
---
+++ process_climate_data.py
@@ -0,0 +1,58 @@
+# import required libraries
+import geopandas as gpd
+import rioxarray as rio
+
+# import climate datasets and national boundaries
+gland = gpd.read_file('data/vectors/greenland/GRL_adm0.shp').to_crs(epsg=4326)
+temp = rio.open_rasterio('data/rasters/greenland/cru_ts4.09.1901.2024.tmp.dat.nc')
+prcp = rio.open_rasterio('data/rasters/greenland/cru_ts4.09.1901.2024.pre.dat.nc')
+
+sland = gpd.read_file('data/vectors/scotland/scotland.shp').to_crs(epsg=27700)
+s_temp = gpd.read_file('data/vectors/scotland/Monthly_Temperature_Observations_1991-2020.shp').to_crs(epsg=27700)
+s_prcp = gpd.read_file('data/vectors/scotland/Monthly_Precipitation_Observations_1991-2020.shp').to_crs(epsg=27700)
+
+# assign the greenland shapefile CRS to climate data
+temp.rio.write_crs(gland.crs, inplace=True)
+prcp.rio.write_crs(gland.crs, inplace=True)
+
+# clip climate data to Greenland boundary
+tclipped = temp.rio.clip(gland.geometry.values, drop=True)
+tclean = tclipped.where(tclipped != tclipped.tmp.rio.nodata)
+
+pclipped = prcp.rio.clip(gland.geometry.values, drop=True)
+pclean = pclipped.where(pclipped != pclipped.pre.rio.nodata)
+
+# select data for the period 1991-2020
+t91_20 = tclean.sel(time=slice('1991-01-01', '2020-12-31'))
+p91_20 = pclean.sel(time=slice('1991-01-01', '2020-12-31'))
+
+# resample to annual data
+annualtmp = t91_20.resample(time='1YE').mean()
+annualprcp = p91_20.resample(time='1YE').sum()
+
+# calculate mean annual temperature and total annual precipitation
+gtemp = annualtmp['tmp'].mean(dim='time')
+gprcp = annualprcp['pre'].mean(dim='time')
+
+# reproject to EPSG:3413
+gtemp = gtemp.rio.reproject("EPSG:3413")
+gprcp = gprcp.rio.reproject("EPSG:3413")
+gprcp = gprcp.where(gprcp > 0)  # remove zero values for precipitation
+
+# write greenland data
+gtemp.rio.to_raster('data/rasters/greenland/temperature.tif')
+gprcp.rio.to_raster('data/rasters/greenland/precipitation.tif')
+
+# clip the UK datasets to the Scotland boundary
+stemp = s_temp.clip(sland)
+sprcp = s_prcp.clip(sland)
+
+# calculate mean temperature and total precipitation for each grid cell
+stemp_values = stemp.select_dtypes(include=['float64', 'int64']).columns
+stemp['mean'] = stemp[stemp_values].mean(axis=1)
+sprcp_values = sprcp.select_dtypes(include=['float64', 'int64']).columns
+sprcp['total'] = sprcp[sprcp_values].sum(axis=1)
+
+# write scottish data to file
+sprcp.to_file('data/vectors/scotland/precipitation.shp')
+stemp.to_file('data/vectors/scotland/temperature.shp')
